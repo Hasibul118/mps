43e3ddcf912a25b3e5e000b7c8e4818046c13d4a
Hasibul Hasan <hasibshanto85@gmail.com>
2026-04-25 17:13:34 +0600

    Project initializing

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Modern Power Solutions",
+  description: "Modern Power Solutions is a premier solar power solution company specializing in sustainable energy solutions for residential and commercial clients. We provide expert installation, maintenance, and consultation services to harness the power of the sun.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
